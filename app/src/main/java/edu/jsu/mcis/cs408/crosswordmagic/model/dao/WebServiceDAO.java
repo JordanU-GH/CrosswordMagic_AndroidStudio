99573189a710e20dcd2efa84cdae0f9380404a14
JordanU-GH <junderwood9@stu.jsu.edu>
2026-04-07 13:41:25 -0500

    Adapted code from web service demo into WebServiceDAO

--- app/src/main/java/edu/jsu/mcis/cs408/crosswordmagic/model/dao/WebServiceDAO.java
+++ app/src/main/java/edu/jsu/mcis/cs408/crosswordmagic/model/dao/WebServiceDAO.java
@@ -1,17 +1,27 @@
 package edu.jsu.mcis.cs408.crosswordmagic.model.dao;
 
-import org.json.JSONArray;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class WebServiceDAO {
     private final String TAG = "WebServiceDAO";
     private final DAOFactory daoFactory;
     private final String HTTP_METHOD = "GET";
-    private final String ROOT_URL = "http://ec2-3-142-171-53.us-east-2.compute.amazonaws.com:8080/CrosswordMagicServer/puzzle";
+    private final String ROOT_URL = "http://ec2-3-137-195-31.us-east-2.compute.amazonaws.com:8080/CrosswordMagicServer/puzzle";
     private String requestUrl;
     private ExecutorService pool;
 
@@ -36,6 +46,68 @@ public class WebServiceDAO {
         public String call() {
             StringBuilder r = new StringBuilder();
             // ... adapt Runnable code from "Web Service Demo" here
+            String line;
+            HttpURLConnection conn = null;
+            JSONObject results = null;
+
+            /* Log Request Data */
+            try {
+                /* Check if task has been interrupted */
+                if (Thread.interrupted())
+                    throw new InterruptedException();
+
+                /* Create Request */
+                URL url = new URL(requestUrl);
+
+                conn = (HttpURLConnection)url.openConnection();
+
+                conn.setReadTimeout(10000); /* ten seconds */
+                conn.setConnectTimeout(15000); /* fifteen seconds */
+
+                conn.setRequestMethod("GET");
+                conn.setDoInput(false);
+
+                /* Send Request */
+                conn.connect();
+
+                /* Check if task has been interrupted */
+                if (Thread.interrupted())
+                    throw new InterruptedException();
+
+                /* Get Reader for Results */
+                int code = conn.getResponseCode();
+
+                if (code == HttpsURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    /* Read Response Into StringBuilder */
+                    do {
+                        line = reader.readLine();
+                        if (line != null)
+                            r.append(line);
+                    }
+                    while (line != null);
+
+                }
+
+                /* Check if task has been interrupted */
+                if (Thread.interrupted())
+                    throw new InterruptedException();
+
+                /* Parse Response as JSON */
+                results = new JSONObject(r.toString());
+                r.append(results);
+
+            }
+            catch (Exception e) {
+                Log.e(TAG, " Exception: ", e);
+            }
+            finally {
+                if (conn != null) { conn.disconnect(); }
+            }
+
+            /* Finished; Log and Return Results */
+            Log.d(TAG, " JSON: " + r.toString());
             return r.toString().trim();
         }
     }
